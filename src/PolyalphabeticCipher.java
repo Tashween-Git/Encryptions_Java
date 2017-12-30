dd909c6ba7f149d367763c973c1089cf86145237
tash <tash1108.s@outlook.com>
2017-12-31 01:04:50 +0400

    re-structured code
    -- With Annotations

--- src/PolyalphabeticCipher.java
+++ src/PolyalphabeticCipher.java
@@ -1,42 +1,60 @@
+import sun.awt.Symbol;
+
+import java.util.Scanner;
+
 public class PolyalphabeticCipher
 {
-    public static String encrypt(String text, final String key)
-    {
-        String res = "";
-        text = text.toUpperCase();
-        for (int i = 0, j = 0; i < text.length(); i++)
-        {
-            char c = text.charAt(i);
-            if (c < 'A' || c > 'Z')
-                continue;
-            res += (char) ((c + key.charAt(j) - 2 * 'A') % 26 + 'A');
-            j = ++j % key.length();
-        }
-        return res;
-    }
-
-    public static String decrypt(String text, final String key)
-    {
-        String res = "";
-        text = text.toUpperCase();
-        for (int i = 0, j = 0; i < text.length(); i++)
-        {
-            char c = text.charAt(i);
-            if (c < 'A' || c > 'Z')
-                continue;
-            res += (char) ((c - key.charAt(j) + 26) % 26 + 'A');
-            j = ++j % key.length();
-        }
-        return res;
-    }
-
+    // Program starts here
     public static void main(String[] args)
     {
-        String key = "VIGENERECIPHER";
-        String message = "Beware the Jabberwock, my son! The jaws that bite, the claws that catch!";
-        String encryptedMsg = encrypt(message, key);
-        System.out.println("String: " + message);
-        System.out.println("Encrypted message: " + encryptedMsg);
-        System.out.println("Decrypted message: " + decrypt(encryptedMsg, key));
+        final String key = "VIGENERECIPHER"; // setting the word 'VIGENERECIPHER' as encryption key
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Text: ");
+        String message = sc.next(); // Getting user input
+        String encryptedText = encrypt(message, key); // calling encryption method and getting encrypted text
+        System.out.println("Entered Text: " + message); // output input text
+        System.out.println("Encrypted text: " + encryptedText); // output encrypted text
+        System.out.println("Decrypted text: " + decrypt(encryptedText, key)); // output decrypted text
     }
+
+    // Encryption Method
+    public static String encrypt(String text, String key)
+    {
+        String res = ""; // variable to store encrypted text
+        text = text.toUpperCase(); // converting whole text to upper case
+        for (int i = 0, j = 0; i < text.length(); i++)
+        {
+            char c = text.charAt(i); // getting character at position i from input text
+            if (c < 'A' || c > 'Z')
+                continue;
+//            System.out.println((c));
+//            System.out.println(key.charAt(j));
+//            System.out.println((c+ key.charAt(j)));
+//            System.out.println((c+ key.charAt(j) - 2 ));
+//            System.out.println(2 * 'A');
+//            System.out.println((c+ key.charAt(j) - 2 * 'A'));
+//            System.out.println((c + key.charAt(j) - 2 * 'A') % 26 + 'A');
+
+            res += (char) ((c + key.charAt(j) - 2 * 'A') % 26 + 'A'); // calculating the encrypted value for the character and concatenating to variable
+            j = ++j % key.length();
+        }
+        return res;
+    }
+
+    // Decryption Method
+    public static String decrypt(String text, String key)
+    {
+        String res = ""; // variable to store decrypted text
+        text = text.toUpperCase(); // converting whole text to upper case
+        for (int i = 0, j = 0; i < text.length(); i++)
+        {
+            char c = text.charAt(i); // getting character at position i from encrypted text
+            if (c < 'A' || c > 'Z')
+                continue;
+            res += (char) ((c - key.charAt(j) + 26) % 26 + 'A'); // calculating the encrypted value for the character and concatenating to variable
+            j = ++j % key.length();
+        }
+        return res;
+    }
+
 }
